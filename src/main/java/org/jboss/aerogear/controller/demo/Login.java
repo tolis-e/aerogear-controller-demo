7d1b60d7d21dba870242d206fd0794fe1fc798e5
Bruno Oliveira <bruno@abstractj.org>
2012-11-13 20:10:27 -0200

    Included secret retrieval

--- src/main/java/org/jboss/aerogear/controller/demo/Login.java
+++ src/main/java/org/jboss/aerogear/controller/demo/Login.java
@@ -17,8 +17,11 @@
 
 package org.jboss.aerogear.controller.demo;
 
+import org.abstractj.cuckootp.Totp;
+import org.abstractj.cuckootp.api.Base32;
 import org.jboss.aerogear.security.auth.AuthenticationManager;
 import org.jboss.aerogear.security.model.AeroGearUser;
+import org.jboss.aerogear.security.persistence.UserRepository;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -29,11 +32,24 @@ public class Login {
     @Inject
     private AuthenticationManager authenticationManager;
 
+    @Inject
+    private UserRepository userRepository;
+
     public void index() {
         System.out.println("Login page!");
     }
 
     public AeroGearUser login(AeroGearUser user) {
+
+        AeroGearUser aeroGearUser = userRepository.findById(user.getId());
+
+        if (aeroGearUser == null) {
+            String secret = new Base32().random();
+            Totp totp = new Totp(secret);
+            user.setSecret(secret);
+            userRepository.store(user);
+        }
+
         authenticationManager.login(user);
         return user;
     }
