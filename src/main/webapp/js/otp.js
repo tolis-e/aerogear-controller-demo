64c7d3f12351d0c81a139e22e320c55560914fc5
Bruno Oliveira <bruno@abstractj.org>
2012-11-24 13:28:00 -0200

    How do relative paths work?

--- src/main/webapp/js/otp.js
+++ src/main/webapp/js/otp.js
@@ -3,7 +3,7 @@ $( document ).ready( function() {
 	$(' #qrcode ').click( function( event ) {
         event.preventDefault();
 
-		$.ajax('/aerogear-controller-demo/auth/otp/secret', {
+		$.ajax('auth/otp/secret', {
 	        data:{},
 	        type:'GET', 
 	        success:function (data) {
