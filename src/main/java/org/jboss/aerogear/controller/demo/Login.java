f10fbb5e9100b4d2addbd94e37fad8b3f7067cfa
Bruno Oliveira <bruno@abstractj.org>
2013-06-14 10:27:06 -0300

    Removal of AeroGearUser reference

--- src/main/java/org/jboss/aerogear/controller/demo/Login.java
+++ src/main/java/org/jboss/aerogear/controller/demo/Login.java
@@ -38,7 +38,7 @@ public class Login {
     }
 
     /**
-     * {@link org.jboss.aerogear.security.model.AeroGearUser} registration
+     * User registration
      *
      *
      * @param user represents a simple implementation that holds user's credentials.
